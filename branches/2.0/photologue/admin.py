0c5255e85e516566349dcb5ca40306faed7afdf7
justin.driscoll <justin.driscoll@2d8b3452-2338-0410-8a60-9550768eb463>
2008-07-08 18:23:22 +0000

    Photologue models are now registered with the global admin site as well as the "photologue/admin/" site.

--- branches/2.0/photologue/admin.py
+++ branches/2.0/photologue/admin.py
@@ -4,21 +4,18 @@
 from django.contrib import admin
 from models import *
 
-
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_added', 'photo_count', 'is_public')
     list_filter = ['date_added', 'is_public']
     date_hierarchy = 'date_added'
     prepopulated_fields = {'title_slug': ('title',)}
-    filter_horizontal = ('photos',)
-    
+    filter_horizontal = ('photos',)    
     
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_taken', 'date_added', 'is_public', 'tags', 'view_count', 'admin_thumbnail')
     list_filter = ['date_added', 'is_public']
     list_per_page = 10
     prepopulated_fields = {'title_slug': ('title',)}
-
     
 class PhotoEffectAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'admin_sample')
@@ -37,7 +34,6 @@ class PhotoEffectAdmin(admin.ModelAdmin):
         }),
     )
 
-    
 class PhotoSizeAdmin(admin.ModelAdmin):
     list_display = ('name', 'width', 'height', 'crop', 'pre_cache', 'effect', 'increment_count')
     fieldsets = (
@@ -51,17 +47,23 @@ class PhotoSizeAdmin(admin.ModelAdmin):
             'fields': ('effect', 'watermark',)
         }),
     )
-
     
 class WatermarkAdmin(admin.ModelAdmin):
     list_display = ('name', 'opacity', 'style')
 
-    
+# register the local admin
 photologue_admin = admin.AdminSite()
-
 photologue_admin.register(Gallery, GalleryAdmin)
 photologue_admin.register(GalleryUpload)
 photologue_admin.register(Photo, PhotoAdmin)
 photologue_admin.register(PhotoEffect, PhotoEffectAdmin)
 photologue_admin.register(PhotoSize, PhotoSizeAdmin)
 photologue_admin.register(Watermark, WatermarkAdmin)
+
+# register the global admin
+admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(GalleryUpload)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(PhotoEffect, PhotoEffectAdmin)
+admin.site.register(PhotoSize, PhotoSizeAdmin)
+admin.site.register(Watermark, WatermarkAdmin)
